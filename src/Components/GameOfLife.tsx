0ca91a8b5029cae0e79ee833b560ea5554fb526f
juanehgr <jahernandezgr@gmail.com>
2024-06-27 19:48:40 +0200

    feat(vite): refactored grid and methods

--- src/Components/GameOfLife.tsx
+++ src/Components/GameOfLife.tsx
@@ -1,66 +1,20 @@
-import React, { useState, useEffect, useRef } from 'react';
-import Cell, { CellState } from './Cell';
+import React, { useEffect, useRef, useState } from 'react';
 import { Grid } from './Grid';
 
-// Definimos el tamaño de la cuadrícula
-const GRID_WIDTH = 40;
-const GRID_HEIGHT = 40;
-
-// Definimos las posibles direcciones de los vecinos
-const NEIGHBOR_OFFSETS = [
-  [-1, -1],
-  [0, -1],
-  [1, -1],
-  [-1, 0],
-  [1, 0],
-  [-1, 1],
-  [0, 1],
-  [1, 1],
-];
-
 const GameOfLife: React.FC = () => {
-
   const grid = useRef(Grid.initialize());
-
-  const calculateNextGeneration = () => {
-    const newGrid = grid.map((row, y) =>
-      row.map((cell, x) => {
-        const livingNeighbors = NEIGHBOR_OFFSETS.reduce((count, [dx, dy]) => {
-          const nx = x + dx;
-          const ny = y + dy;
-          if (nx >= 0 && nx < GRID_WIDTH && ny >= 0 && ny < GRID_HEIGHT) {
-            return count + (grid[ny][nx].askIfAlive() ? 1 : 0);
-          }
-          return count;
-        }, 0);
-
-        if (
-          cell.askIfAlive() &&
-          (livingNeighbors === 2 || livingNeighbors === 3)
-        ) {
-          return new Cell(CellState.ALIVE);
-        }
-
-        if (cell.askIfDead() && livingNeighbors === 3) {
-          return new Cell(CellState.ALIVE);
-        }
-
-        return new Cell(CellState.DEAD);
-      }),
-    );
-    setGrid(newGrid);
-  };
-
+  const [cells, setCells] = useState(grid.current.getCells());
   useEffect(() => {
     const intervalId = setInterval(() => {
-      calculateNextGeneration();
+      grid.current.calculateNextGeneration();
+      setCells(grid.current.getCells());
     }, 100);
     return () => clearInterval(intervalId);
   }, [grid]);
 
   return (
     <div>
-      {grid.map((row, y) => (
+      {cells.map((row, y) => (
         <div key={y} style={{ display: 'flex' }}>
           {row.map((cell, x) => (
             <div
@@ -68,7 +22,7 @@ const GameOfLife: React.FC = () => {
               style={{
                 width: 20,
                 height: 20,
-                backgroundColor: cell ? 'green' : 'white',
+                backgroundColor: cell.askIfAlive() ? 'green' : 'white',
                 border: '1px solid black',
               }}
             />
--- src/Components/Grid.ts
+++ src/Components/Grid.ts
@@ -1,4 +1,16 @@
-import Cell from './Cell';
+import Cell, { CellState } from './Cell';
+
+// Definimos las posibles direcciones de los vecinos
+const NEIGHBOR_OFFSETS = [
+  [-1, -1],
+  [0, -1],
+  [1, -1],
+  [-1, 0],
+  [1, 0],
+  [-1, 1],
+  [0, 1],
+  [1, 1],
+];
 
 export class Grid {
   static height = 40;
@@ -10,5 +22,42 @@ export class Grid {
       Array.from({ length: this.width }, () => Cell.newRandom()),
     );
     return new Grid(cells);
+  }
+
+  getCells(): Cell[][] {
+    return this.cells;
+  }
+
+  public calculateNextGeneration() {
+    const newGrid = this.cells.map((row, y) =>
+      row.map((cell, x) => {
+        const livingNeighbors = this.getLiveNeighbors(x, y);
+
+        if (
+          cell.askIfAlive() &&
+          (livingNeighbors === 2 || livingNeighbors === 3)
+        ) {
+          return new Cell(CellState.ALIVE);
+        }
+
+        if (cell.askIfDead() && livingNeighbors === 3) {
+          return new Cell(CellState.ALIVE);
+        }
+
+        return new Cell(CellState.DEAD);
+      }),
+    );
+    this.cells = newGrid;
+  }
+
+  private getLiveNeighbors(x: number, y: number): number {
+    return NEIGHBOR_OFFSETS.reduce((count, [dx, dy]) => {
+      const nx = x + dx;
+      const ny = y + dy;
+      if (nx >= 0 && nx < Grid.height && ny >= 0 && ny < Grid.width) {
+        return count + (this.cells[ny][nx].askIfAlive() ? 1 : 0);
+      }
+      return count;
+    }, 0);
   }
 }
